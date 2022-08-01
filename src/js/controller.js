064e9bb08ec5fbe84d095b5d459de02df328acf1
Andres David Blandon <dandresavid@hotmail.com>
2022-08-01 12:19:48 -0500

    Added pagination feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,17 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 import { async } from 'regenerator-runtime';
+
+// if(module.hot) {
+//   module.hot.accprt();
+// }
 
 const controlRecipes = async function() {
   try{
@@ -26,6 +33,8 @@ const controlRecipes = async function() {
 
 const controlSearchResult = async function() {
   try{
+    resultsView.renderSpinner();
+
     //1) Get search query
     const query = searchView.getQuery();
     if(!query) return;
@@ -34,17 +43,27 @@ const controlSearchResult = async function() {
     await model.loadSearchResults(query);
 
     // 3 Render results
-    console.log(model.state.search.results)
+    resultsView.render(model.getSearchResultsPage());
+
+    //4 Render initial pagination buttons
+    paginationView.render(model.state.search)
 
   } catch (err){
     console.log(err)
   }
 }
-controlSearchResult();
+const controlPagination = function(goToPage){
+    // 1 Render  new results
+    resultsView.render(model.getSearchResultsPage(goToPage));
+
+    //2 Render new pagination buttons
+    paginationView.render(model.state.search);
+}
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
-  searchView.addHandlerSearch(controlSearchResult)
+  searchView.addHandlerSearch(controlSearchResult);
+  paginationView.addHandlerClick(controlPagination)
 };
 
 init();
